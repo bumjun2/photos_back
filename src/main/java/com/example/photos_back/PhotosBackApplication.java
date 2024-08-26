19164e29c8ed8a41047810ab4a13d8089e73d809
bumjun2 <jbj3713@naver.com>
2024-08-26 22:38:54 +0900

    Initial commit

---
+++ src/main/java/com/example/photos_back/PhotosBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.photos_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotosBackApplication.class, args);
+    }
+
+}
