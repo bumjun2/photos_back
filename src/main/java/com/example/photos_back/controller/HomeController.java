c0b7a57ad2f1d83257416ca1776a56f6d1a26dd3
bumjun2 <jbj3713@naver.com>
2024-09-01 19:24:58 +0900

    REST API 사용 예쩡

--- src/main/java/com/example/photos_back/controller/HomeController.java
+++ src/main/java/com/example/photos_back/controller/HomeController.java
@@ -1,16 +1,22 @@
 package com.example.photos_back.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
     @GetMapping("")
-    public String MyHome(){
+    @ResponseBody
+    public ResponseEntity<?> MyHome(){
         System.out.println("method : get comment : weclome to home");
-        return "/home";
+        return ResponseEntity.ok()
+                .body(
+                        "정범준바보"
+                );
     }
 }
